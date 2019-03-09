4f3708207d87d1a25fdb7160971f94d94caa711c
gbarrbrown <gbb7899@gmail.com>
2019-03-10 08:34:54 +1300

    create getTodos db fn

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 const connection = require('./connection')
 
 module.exports = {
-
+    getTodos: getTodos,
 }
 // write functions below
 
+function getTodos(testDb) {
+    const db = testDb || connection
+
+    return db('todos').select()
+}
