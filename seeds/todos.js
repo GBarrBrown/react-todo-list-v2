df8bb88aad97b7f22146628e10a81e8cc74e1120
gbarrbrown <gbb7899@gmail.com>
2019-03-10 08:22:38 +1300

    create todos seeds

---
+++ seeds/todos.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('todos').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('table_name').insert([
+        {id: 1, title: 'Finish Todo List Project', completed: false},
+        {id: 2, title: 'Create A Boilerplate', completed: false},
+        {id: 3, title: 'Begin Todo List Project', completed: true}
+      ]);
+    });
+};
