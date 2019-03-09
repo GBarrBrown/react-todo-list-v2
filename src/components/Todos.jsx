8f78c6978d31ecff697904d0138fc04ccd960726
gbarrbrown <gbb7899@gmail.com>
2019-03-09 17:50:54 +1300

    add contructor & componentDidMount

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
 class Todos extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos : []
+        }
+    }
+
+    componentDidMount(){
+        // fetch all todos from db
+    }
+
     render() {
         return (
             <div className="Todo-container">
